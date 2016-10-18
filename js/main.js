160ddd41ef2bdc5e92959832c93c45e964c535af
Ryan Lewis <r2c2ryan@gmail.com>
2016-10-18 00:01:02 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+ // console.log("Page Controller reporting for duty.");
+
+
+
+
+});
