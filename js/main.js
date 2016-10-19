c6b4ff47d9c748bc06bf0bc5f932f3500041b68e
rlewis <rlewis@RYAN-7>
2016-10-19 14:06:30 -0700

    -Fix home text -fix bootstrap menu close on click

--- js/main.js
+++ js/main.js
@@ -46,3 +46,10 @@ app.controller('FooterCtrl', function ($scope) {
 
 
 });
+
+
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a')) {
+        $(this).collapse('hide');
+    }
+});
