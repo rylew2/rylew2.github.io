1f26fd0c7bc47f31566ee4b776446751121a469d
rlewis <rlewis@RYAN-7>
2016-10-28 16:34:33 -0700

    refactor css and js into their own files

---
+++ js/githubBrowser-d3LanguageTotals.js
@@ -0,0 +1,129 @@
+﻿
+function render(dataset) {
+
+    // Dimensions for the chart: height, width, and space b/t the bars
+    var margins = { top: 30, right: 50, bottom: 30, left: 50 }
+    var height = 400 - margins.left - margins.right,
+        width = 700 - margins.top - margins.bottom,
+        barPadding = 5
+
+    // Create a scale for the y-axis based on data
+    // >> Domain - min and max values in the dataset
+    // >> Range - physical range of the scale (reversed)
+    var yScale = d3.scale.linear()
+      .domain([0, d3.max(dataset, function (d) {
+          return d.snow;
+      })])
+      .range([height, 0]);
+
+    // Implements the scale as an actual axis
+    // >> Orient - places the axis on the left of the graph
+    // >> Ticks - number of points on the axis, automated
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient('left')
+      .ticks(5);
+
+    // Creates a scale for the x-axis based on city names
+    var xScale = d3.scale.ordinal()
+      .domain(dataset.map(function (d) {
+          return d.city;
+      }))
+      .rangeRoundBands([0, width], .1);
+
+    // Creates an axis based off the xScale properties
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('bottom');
+
+    // Creates the initial space for the chart
+    // >> Select - grabs the empty <div> above this script
+    // >> Append - places an <svg> wrapper inside the div
+    // >> Attr - applies our height & width values from above
+    var chart = d3.select('.barChart')
+      .append('svg').attr('class', 'svgBarChart')
+      .attr('width', width + margins.left + margins.right)
+      .attr('height', height + margins.top + margins.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+
+
+    var tooltip = d3.select('body').append('div')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0)
+
+
+    // For each value in our dataset, places and styles a bar on the chart
+
+    // Step 1: selectAll.data.enter.append
+    // >> Loops through the dataset and appends a rectangle for each value
+    chart.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+        .attr('class', 'barChartMouseover()')
+      // Step 2: X & Y
+      // >> X - Places the bars in horizontal order, based on number of
+      //        points & the width of the chart
+      // >> Y - Places vertically based on scale
+      .attr('x', function (d, i) {
+          return xScale(d.city);
+      })
+      .attr('y', function (d) {
+          return yScale(d.snow);
+      })
+      .on('mouseover', function (d) {
+
+          tooltip.transition()
+              .style('opacity', .7)
+
+          tooltip.html(d.snow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' lines')
+              .style('left', (d3.event.pageX - 35) + 'px')
+              .style('top', (d3.event.pageY - 30) + 'px')
+
+      })
+      .on('mouseout', function (d) {
+
+          tooltip.transition()
+              .style('opacity', 0)
+      })
+
+
+
+      // Step 3: Height & Width
+      // >> Width - Based on barpadding and number of points in dataset
+      // >> Height - Scale and height of the chart area
+      .attr('width', (width / dataset.length) - barPadding)
+      .attr('height', function (d) {
+          return height - yScale(d.snow);
+      })
+      .attr('fill', 'steelblue')
+
+      // Step 4: Info for hover interaction
+      .attr('class', function (d) {
+          return d.city;
+      })
+      .attr('id', function (d) {
+          return d.snow;
+      });
+
+    // Renders the yAxis once the chart is finished
+    // >> Moves it to the left 10 pixels so it doesn't overlap
+    chart.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(-10, 0)')
+      .call(yAxis);
+
+    // Appends the yAxis
+    chart.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(0,' + (height + 10) + ')')
+      .call(xAxis);
+
+    // Adds yAxis title
+    chart.append('text')
+      .text('Lines of Code')
+      .attr('transform', 'translate(-70, -20)');
+}
---
+++ js/githubBrowser-d3RepoLanguages.js
@@ -0,0 +1,222 @@
+﻿d3.select("input[value=\"total\"]").property("checked", true);
+
+var svg = d3.select("#chart")
+    .append("svg").attr("ng-show", "chartMessage==null")
+    .append("g")
+
+svg.append("g")
+    .attr("class", "slices");
+svg.append("g")
+    .attr("class", "labelName");
+svg.append("g")
+    .attr("class", "labelValue");
+svg.append("g")
+    .attr("class", "lines");
+
+var width = 960 / 2,
+    height = 450 / 2,
+    radius = (Math.min(width, height) / 2) + 20;
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function (d) {
+        return d.value;
+    });
+
+var arc = d3.svg.arc()
+    .outerRadius(radius * 0.8)
+    .innerRadius(radius * 0.4);
+
+var outerArc = d3.svg.arc()
+    .innerRadius(radius * 0.9)
+    .outerRadius(radius * 0.9);
+
+var legendRectSize = (radius * 0.05);
+var legendSpacing = radius * 0.02;
+
+
+var div = d3.select("body").append("div").attr("class", "toolTip");
+
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var colorRange = d3.scale.category20();
+var color = d3.scale.ordinal()
+    .range(colorRange.range());
+
+
+function change(data) {
+
+    /* ------- PIE SLICES -------*/
+    var slice = svg.select(".slices").selectAll("path.slice")
+        .data(pie(data), function (d) { return d.data.label });
+
+    slice.enter()
+        .insert("path")
+        .style("fill", function (d) { return color(d.data.label); })
+        .attr("class", "slice");
+
+    slice
+        .transition().duration(1000)
+        .attrTween("d", function (d) {
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function (t) {
+                return arc(interpolate(t));
+            };
+        })
+    slice
+        .on("mousemove", function (d) {
+            div.style("left", d3.event.pageX + 10 + "px");
+            div.style("top", d3.event.pageY - 25 + "px");
+            div.style("display", "inline-block");
+            div.html((d.data.label) + "<br>" + (d.data.value) + "%");
+        });
+    slice
+        .on("mouseout", function (d) {
+            div.style("display", "none");
+        });
+
+    slice.exit()
+        .remove();
+
+
+
+    /* ------- SLICE TO TEXT POLYLINES -------*/
+
+    var polyline = svg.select(".lines").selectAll("polyline")
+        .data(pie(data), function (d) { return d.data.label });
+
+    polyline.enter()
+        .append("polyline");
+
+    polyline.transition().duration(1000)
+        .attrTween("points", function (d) {
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function (t) {
+                var d2 = interpolate(t);
+                var pos = outerArc.centroid(d2);
+                pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                return [arc.centroid(d2), outerArc.centroid(d2), pos];
+            };
+        });
+
+    polyline.exit()
+        .remove();
+
+
+
+    /* ------- TEXT LABELS -------*/
+
+    var text = svg.select(".labelName").selectAll("text")
+        .data(pie(data), function (d) { return d.data.label });
+
+    text.enter()
+        .append("text")
+        .attr("dy", ".35em")
+        .text(function (d) {
+            return (d.data.label + ": " + d.value + "%");
+
+        });
+    //.attr("transform", function (d) {
+    //    return "translate(" + pos.centroid(d) + ") " +
+    //            "rotate(" + getAngle(d) + ")";
+    //})             ;
+
+    function midAngle(d) {
+        return d.startAngle + (d.endAngle - d.startAngle) / 2;
+    }
+
+    text
+        .transition().duration(1000)
+        .attrTween("transform", function (d) {
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function (t) {
+                var d2 = interpolate(t);
+                var pos = outerArc.centroid(d2);
+                pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+                return "translate(" + pos + ")";
+            };
+        })
+        .styleTween("text-anchor", function (d) {
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function (t) {
+                var d2 = interpolate(t);
+                return midAngle(d2) < Math.PI ? "start" : "end";
+            };
+        })
+        .text(function (d) {
+            return (d.data.label + ": " + d.value + "%");
+        });
+
+
+    text.exit()
+        .remove();
+
+
+    alpha = .7;
+    spacing = 11;
+
+    function relax() {
+        again = false;
+        text.each(function (d, i) {
+            a = this; //reference to text label
+            da = d3.select(a);
+            y1 = da.attr("y");
+            text.each(function (d, j) {
+                b = this;
+                // a & b are the same element and don't collide.
+                if (a == b) return;
+                db = d3.select(b);
+                // a & b are on opposite sides of the chart and
+                // don't collide
+                if (da.attr("text-anchor") != db.attr("text-anchor")) return;
+                // Now let's calculate the distance between
+                // these elements. 
+                y2 = db.attr("y");
+                deltaY = y1 - y2;
+
+                // If spacing is greater than our specified spacing,
+                // they don't collide.
+                if (Math.abs(deltaY) > spacing) return;
+
+                // If the labels collide, we'll push each 
+                // of the two labels up and down a little bit.
+                again = true;
+                sign = deltaY > 0 ? 1 : -1;
+                adjust = sign * alpha;
+                da.attr("y", +y1 + adjust);
+                db.attr("y", +y2 - adjust);
+            });
+
+        });
+        // Adjust our line leaders here
+        // so that they follow the labels. 
+        if (again) {
+            //debugger;
+            labelElements = text[0];
+            //  debugger;
+            polyline.attr("y2", function (d, i) {
+                //  debugger;
+                labelForLine = d3.select(labelElements[i]);
+                return labelForLine.attr("y");
+            });
+
+
+
+
+            setTimeout(relax, 20)
+        }
+    }
+
+    relax();
+
+
+
+};
