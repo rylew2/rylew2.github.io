35139b58cad07543a5c70101f0325eded0cf25f1
rlewis <rlewis@RYAN-7>
2016-10-28 17:51:04 -0700

    refactor controller js into their own files

---
+++ js/githubBrowser/MainController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    /* Main Controller*/
+    app.controller('MainController', function ($scope, $interval, $location, $routeParams) {
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data from GitHub";
+        };
+
+        $scope.search = function (username) {
+            $location.path("/ghv/user/" + username);
+        };
+    });
+    /* End Main Controller */
+
+
+    var myapp = angular.module("rylew"); //just a reference to GitHubViewer not an instantiation/creation
+
+    myapp.controller("UserController", UserController);
+
+})();
---
+++ js/githubBrowser/RepoController.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+
+
+    /*Repo Controller*/
+    app.controller('RepoController', function ($scope, github, $routeParams) {
+
+        var onRepo = function (data) {
+            $scope.repo = data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = reason;
+        };
+
+        //username and repo passed from user.html
+        var reponame = $routeParams.reponame;
+        var username = $routeParams.username;
+
+        github.getRepoDetails(username, reponame)
+        .then(onRepo, onError);
+
+    });
+    /* End Repo Controller*/
+
+
+
+    var myapp = angular.module("rylew"); //just a reference to GitHubViewer not an instantiation/creation
+
+    myapp.controller("RepoController", UserController);
+
+})();
