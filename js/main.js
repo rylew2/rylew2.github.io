4249c5a6405652099ef39c35f76d46823c8ea670
r2c2ryan@gmail.com <r2c2ryan@gmail.com>
2016-10-31 23:54:58 -0700

    start to try to change height of navbar on scroll

--- js/main.js
+++ js/main.js
@@ -60,3 +60,11 @@
             $(this).collapse('hide');
         }
     });
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+            $('nav').addClass('shrink');
+        } else {
+            $('nav').removeClass('shrink');
+        }
+    });
