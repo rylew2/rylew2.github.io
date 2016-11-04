99934077ebe7015eff016d4725887bba4abfdbe3
rlewis <rlewis@RYAN-7>
2016-11-03 18:04:21 -0700

    -setup dimple file add/delete chart

--- js/githubBrowser/RepoController.js
+++ js/githubBrowser/RepoController.js
@@ -1,19 +1,39 @@
 ﻿(function () {
 
     /*Repo Controller*/
-    app.controller('RepoController', function ($scope, github, $routeParams, $http) {
+    app.controller('RepoController', function ($scope, github, $routeParams, $http, $timeout) {
+
+     
+
 
         var onContributors = function (data) {
+
+
             $scope.repo = data;
+          //  debugger;
             $scope.contributorTotal = data.contributors.length;
             $scope.loading = null;
 
             var clientSecret = '?client_id=9b254584c151259f146a&client_secret=552ead4d53b54ebd99adf72edd017626c5f496cf';
             var langURL = "https://api.github.com/repos/" + username + "/" + reponame + '/stats/contributors' + clientSecret;
-            $http.get(langURL).then(onStats, onError);
+            $http.get(langURL).then(function (response) {
+                if (response.status == 202) {
+                  //  debugger;
+              
+                    $timeout(function () {
+                        $scope.chartMessage = "Waiting on Github to compute stats...";
+                        $http.get(langURL).then(onStats, onError);
+                    }, 3000);
+                   
+                }
+                else if (response.status == 200) {
+                    onStats(response);
+                }
+            });
         };
 
         var onStats = function (data) {
+           $scope.chartMessage = "resolved"
             $scope.commitStats = [];
             var responseData = data.data;
             var obj = {};
@@ -26,19 +46,43 @@
                 var weeks = responseData[i].weeks;
 
                 for (var j = 0; j < responseData[i].weeks.length; j++) {
-
-                    obj.a = obj.a + weeks[j].a;
-                
-                    obj.d = obj.d + weeks[j].d;
-                    
+                    obj.a = obj.a + weeks[j].a;               
+                    obj.d = obj.d + weeks[j].d;                    
                 }
                 $scope.commitStats.push(obj);
-                
+
             }
 
-            var zzzz = 2;
+            var chartData = [];
+            for (var i = $scope.commitStats.length-1; (i > ($scope.commitStats.length -6)) ; i--) {
 
-        };
+     
+                if ( ($scope.commitStats[i] != undefined) && i>=0) {
+                    chartData.push(
+                        {
+                            Tasks: $scope.commitStats[i].a.toString(),
+                            User: $scope.commitStats[i].author,
+                            MyValue: "Additions"
+                        }
+                    );
+
+                    chartData.push(
+                     {
+                         Tasks: $scope.commitStats[i].d.toString(),
+                         User: $scope.commitStats[i].author.toString(),
+                         MyValue: "Deletions"
+                     }
+                     );
+
+                    
+
+                }            
+            }
+           
+            dimpleStacked(chartData);
+
+
+        }; //end onStats
 
         var onError = function (reason) {
             $scope.error = reason;
@@ -54,18 +98,18 @@
 
 
         /* Convert unix time from github api to date */
-        function timeConverter(UNIX_timestamp) {
-            var a = new Date(UNIX_timestamp * 1000);
-            var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-            var year = a.getFullYear();
-            var month = months[a.getMonth()];
-            var date = a.getDate();
-            var hour = a.getHours();
-            var min = a.getMinutes();
-            var sec = a.getSeconds();
-            var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-            return time;
-        }
+        //function timeConverter(UNIX_timestamp) {
+        //    var a = new Date(UNIX_timestamp * 1000);
+        //    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        //    var year = a.getFullYear();
+        //    var month = months[a.getMonth()];
+        //    var date = a.getDate();
+        //    var hour = a.getHours();
+        //    var min = a.getMinutes();
+        //    var sec = a.getSeconds();
+        //    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+        //    return time;
+        //}
 
 
 
---
+++ js/githubBrowser/githubBrowser-d3RepoStats.js
@@ -0,0 +1,49 @@
+﻿var svg = dimple.newSvg('#chart', "100%", "85%");
+
+function dimpleStacked(data) {
+   // debugger;
+
+    //d3.tsv("/content/example_data.tsv", function (data) {
+    var myChart = new dimple.chart(svg, data);
+    //myChart.setBounds(60, 30, 900, 400);
+
+    var x = myChart.addCategoryAxis("x", "User");
+    //x.addOrderRule(['Jan', 'Feb', 'Mar', 'Apr']);
+    var y = myChart.addMeasureAxis("y", "Tasks");
+
+    // In order to deal with cases where order differs by column 
+    // you need to include it in your series definition
+    var s = myChart.addSeries(["Tasks", "MyValue" ], dimple.plot.bar);
+
+    s.getTooltipText = function (e) {
+      //  debugger;
+        var AorD = e.aggField[1] == "Additions" ? "Additions" : "Deletions";
+
+        return [
+            e.cx + " had " + e.aggField[0] + " " + AorD
+        ];
+    };
+    d3.svg.axis()
+    .tickFormat(d3.format("d"));
+
+    // Your function approach would also work, but as it is the default 
+    // handling for a numeric column you can just specify order like this
+    s.addOrderRule("MyValue");
+
+    y.showPercent = false;
+    y.hidden = false;
+   // y.overrideMax = 500;
+    myChart.assignColor('Additions', '#c1f4cb');
+    myChart.assignColor('Deletions', '#f2bba2');
+
+    myChart.addLegend(60, 10, 510, 20, "right");
+    myChart.draw();
+
+    // If you don't want the order to show in the tooltip
+    // the easiest way is to remove it here once the chart is drawn
+    s.categoryFields = ["User"];
+
+
+
+
+}
