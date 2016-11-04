770f3369bca12f8ff39bf36344ad049ce35c44ab
rlewis <rlewis@RYAN-7>
2016-11-04 12:40:50 -0700

    Fix main controller back to normal

--- js/githubBrowser/MainController.js
+++ js/githubBrowser/MainController.js
@@ -1,17 +1,20 @@
 ﻿(function () {
 
-    //Revealing module pattern
-    //Get user and repos for user
-    var github = function ($http, $timeout) {
+    /* Main Controller*/
+    app.controller('MainController', function ($scope, $interval, $location, $routeParams) {
 
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data from GitHub";
+        };
 
-    }
+        $scope.search = function (username) {
+            $location.path("/ghv/user/" + username);
+        };
+    });
+    /* End Main Controller */
 
-
-
-    var module = angular.module("rylew"); //no second parameter because we're not creating a module, just trying to create a module
-    module.factory("github", github); //register the service with angular
-    // (name of service , name of function that returns function api object ) 
+    //var module = angular.module("rylew"); //no second parameter because we're not creating a module, just trying to create a module
+    //app.controller("MainController", MainController);
 
 
 
