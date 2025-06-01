0cec2baef3245fc2a57a9b34ce9062e872e5d4e4
rylew2 <ryanlewis312@gmail.com>
2025-05-31 22:43:59 -0400

    replace all damp-garden references

--- js/githubBrowser/githubService.js
+++ js/githubBrowser/githubService.js
@@ -1,86 +1,65 @@
 ﻿(function () {
+  
+  var github = function ($http) {
+    
+    // Single base URL for your custom API
+    var apiBase = "https://github-browser-express-backend.vercel.app";
+    
+    // Return GitHub user profile
+    var getUser = function (username) {
+      return $http.get(apiBase + "/user", { params: { user: username } })
+        .then(function (response) {
+          return response.data;
+        });
+    };
 
-    //Revealing module pattern
-    //Get user and repos for user
-    var github = function ($http) {
-      
+    // Return repos for user
+    var getRepos = function (user) {
+      return $http.get(apiBase + "/repo", { params: { user: user.login } })
+        .then(function (response) {
+          return response.data;
+        });
+    };
 
-        //Return github user profile
-        var getUser = function (username) {
-            // return $http.get("https://api.github.com/users/" + username )
-            return $http.get("https://damp-garden-90999.herokuapp.com/user?user=" + username)
-                  .then(function (response) {
-                      return response.data;
-                  });
-        }
+    // Return repo details (with contributors)
+    var getRepoDetails = function (username, reponame) {
+      var repoUrl = apiBase + "/repodetails";
+      var contributorsUrl = apiBase + "/contributors";
 
-        //return repos for user
-        var getRepos = function (user) {
-          //  "repos_url": "https://api.github.com/users/rylew2/repos",
-           //return $http.get(user.repos_url )
-           return $http.get("https://damp-garden-90999.herokuapp.com/repo?user=" + user.login)
-                        .then(function (response) {
-                            return response.data;
-                        });
-        };
+      return $http.get(repoUrl, { params: { user: username, reponame: reponame } })
+        .then(function (response) {
+          var repo = response.data;
+          return $http.get(contributorsUrl, { params: { user: username, reponame: reponame } })
+            .then(function (response) {
+              repo.contributors = response.data;
+              return repo;
+            });
+        });
+    };
 
+    // Return languages for a repo
+    var getLanguages = function (langURL) {
+      var parts = langURL.split('/');
+      var user = parts[4];
+      var repo = parts[5];
+      var newLangURL = apiBase + "/languages";
 
-        var getRepoDetails = function (username, reponame) {
+      return $http.get(newLangURL, { params: { user: user, repo: repo } })
+        .then(function (response) {
+          return response.data;
+        });
+    };
 
-            var repo;
-            //var repoUrl = "https://api.github.com/repos/" + username + "/" + reponame;
-            var repoUrl = "https://damp-garden-90999.herokuapp.com/repodetails?user=" + username + '&reponame=' + reponame;
+    return {
+      getUser: getUser,
+      getRepos: getRepos,
+      getRepoDetails: getRepoDetails,
+      getLanguages: getLanguages
+    };
 
-            //Chained promises
-            //each return is the input to the next success function
-            return $http.get(repoUrl)
-                        .then(function (response) {
-                            repo = response.data;
-                            // return $http.get(repoUrl + "/contributors" );
-                            var contributorsURL = "https://damp-garden-90999.herokuapp.com/contributors?user=" + username + '&reponame=' + reponame;
-                            return $http.get(contributorsURL);
+  };
 
-                        })
-                        .then(function (response) {
-                            repo.contributors = response.data;
-                            return repo;
-                        });
-        };
-
-
-        //return repos for user
-        var getLanguages = function (langURL) {
-            //https://api.github.com/repos/A/accent/languages
-            var spliturl = langURL.split('/');
-            var user = spliturl[4];
-            var repo = spliturl[5];
-            var newLangURL = "https://damp-garden-90999.herokuapp.com/languages?user=" + user + '&repo=' + repo;
-
-
-            // return $http.get(langURL )
-            return $http.get(newLangURL)
-                .then(function (response) {
-                    return response.data;
-             });
-        };
-
-        return {
-            getUser: getUser,
-            getRepos: getRepos,
-            getRepoDetails: getRepoDetails,
-            getLanguages: getLanguages
-        };
-
-    }
-
-
-
-    var module = angular.module("rylew"); //no second parameter because we're not creating a module, just trying to create a module
-    module.factory("github", github); //register the service with angular
-    // (name of service , name of function that returns function api object ) 
-
-
-
-
+  var module = angular.module("rylew");
+  module.factory("github", github);
 
 })();
